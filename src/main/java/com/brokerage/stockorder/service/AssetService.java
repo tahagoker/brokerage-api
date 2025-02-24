5646903a07bc4229fad8350c066c6ac3871a2641
Mustafa Taha Goker <tahagoker@gmail.com>
2025-02-24 10:19:59 +0300

    refactor: improve code formatting and consistency

--- src/main/java/com/brokerage/stockorder/service/AssetService.java
+++ src/main/java/com/brokerage/stockorder/service/AssetService.java
@@ -15,53 +15,56 @@ import java.util.List;
 
 @Service
 public class AssetService {
-    @Autowired
-    private AssetRepository assetRepository;
-    @Autowired
-    private CustomerService customerService;
 
-    public Asset depositMoneyAsset(String customerId, BigDecimal size){
-        Asset asset = getTRYAsset(customerId);
-        if (asset != null) {
-            asset.deposit(size);
-            return assetRepository.save(asset);
-        }
-        return this.createAsset(customerId, Assets.TRY.name(), size);
-    }
+  @Autowired
+  private AssetRepository assetRepository;
+  @Autowired
+  private CustomerService customerService;
 
-    public Asset withdrawMoneyAsset(String customerId, BigDecimal size){
-        Asset asset = getTRYAsset(customerId);
-        if (asset != null && asset.withdraw(size)) {
-            return assetRepository.save(asset);
-        }
-        throw new BaseException("Insufficient funds", HttpStatus.NOT_ACCEPTABLE);
+  public Asset depositMoneyAsset(String customerId, BigDecimal size) {
+    Asset asset = getTRYAsset(customerId);
+    if (asset != null) {
+      asset.deposit(size);
+      return assetRepository.save(asset);
     }
-    public Asset createAsset(String customerId, String assetName, BigDecimal size) {
-        Asset asset = Asset.builder().customer(new Customer(customerId)).assetName(assetName).size(size)
-                .usableSize(size).build();
-        return assetRepository.save(asset);
-    }
+    return this.createAsset(customerId, Assets.TRY.name(), size);
+  }
 
-    public void updateAsset(Asset asset) {
-       assetRepository.save(asset);
+  public Asset withdrawMoneyAsset(String customerId, BigDecimal size) {
+    Asset asset = getTRYAsset(customerId);
+    if (asset != null && asset.withdraw(size)) {
+      return assetRepository.save(asset);
     }
+    throw new BaseException("Insufficient funds", HttpStatus.NOT_ACCEPTABLE);
+  }
 
-    public Asset getAsset(String customerId, String assetName) {
-        Customer customer = customerService.getCustomer(customerId);
-        if (customer != null) {
-            return customerService.getCustomer(customerId).getAssets().stream()
-                    .filter(customerAsset -> customerAsset.getAssetName().equals(assetName)).findAny()
-                    .orElse(null);
-        }
-        return null;
-    }
+  public Asset createAsset(String customerId, String assetName, BigDecimal size) {
+    Asset asset = Asset.builder().customer(new Customer(customerId)).assetName(assetName).size(size)
+        .usableSize(size).build();
+    return assetRepository.save(asset);
+  }
 
-    public Asset getTRYAsset(String customerId) {
-        return getAsset(customerId, Assets.TRY.name());
-    }
+  public void updateAsset(Asset asset) {
+    assetRepository.save(asset);
+  }
 
-    public List<Asset> listAssets(String customerId) {
-        Example<Asset> assetExample = Example.of(Asset.builder().customer(new Customer(customerId)).build());
-        return assetRepository.findAll(assetExample);
+  public Asset getAsset(String customerId, String assetName) {
+    Customer customer = customerService.getCustomer(customerId);
+    if (customer != null) {
+      return customerService.getCustomer(customerId).getAssets().stream()
+          .filter(customerAsset -> customerAsset.getAssetName().equals(assetName)).findAny()
+          .orElse(null);
     }
+    return null;
+  }
+
+  public Asset getTRYAsset(String customerId) {
+    return getAsset(customerId, Assets.TRY.name());
+  }
+
+  public List<Asset> listAssets(String customerId) {
+    Example<Asset> assetExample = Example.of(
+        Asset.builder().customer(new Customer(customerId)).build());
+    return assetRepository.findAll(assetExample);
+  }
 }
